eafc5107f0b2f255e67bd56dfe7f6f6bb88fdf39
ml22826 <149094219+ml22826@users.noreply.github.com>
2024-01-29 17:16:24 +0000

    Add files via upload

---
+++ varients1.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -pe smp 16
+#$ -l h_rt=24:0:0
+#$ -l h_vmem=6G
+#$ -m bea
+
+module load bcftools
+
+bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\n' ~/2024-1-29-Ubuntu/input/chr1.vcf.gz > ~/2024-1-29-Ubuntu/results/varients1.csv 
